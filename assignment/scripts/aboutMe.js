8bec03317446ad5844e50a9fcdbcdbc11b9b0d30
Ben Rehmann <brehmann@me.com>
2023-08-17 12:12:37 -0500

    first save

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,23 +1,31 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Frodo';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Baggins';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
+let fullName = firstName + ' ' + lastName;
 //     Console log the value of `fullName`
+console.log( fullName );
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+let luckyNumber = 9;
 //     Console log the value of `luckyNumber`
+console.log( luckyNumber );
 
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
+let introduction = 'My name is ' + fullName + ' and I think ' + luckyNumber + ' is a winner!';
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
+console.log( introduction );
 
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+let adventurous = true;
 //     Console log the value of `adventurous`
+console.log( adventurous );
 
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
 //     Console log the value of `food`
